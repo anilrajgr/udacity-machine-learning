dfafa663eff3b5445e41d4a8490a1e5b8377cc65
Anil Raj Gopalakrishnan <angopala@microsoft.com>
2017-08-29 12:31:35 -0700

    Temp

---
+++ projects/customer_segments/customer_segments.py
@@ -0,0 +1,30 @@
+"""
+Machine Learning Engineer Nanodegree
+Unsuperviced Learning
+Project: Creating Customer Segments
+
+Welcom to the third project of the Machine Learning Engineer Nanodegree! In this notebook, some template code has already been
+provided for you, and it will be your job to implement the additional functionality necessary to successfully complete this project.
+Sections that being with 'Implementation' in the header indicate that the following block of code will require additonal functionality
+which you must provide. Instructions will be provided for each section and the specifics of the implentation are marked in the code
+block with 'TODO' statement. Please be sure to read teh isntrctions carefull!
+
+In addition to implementing code, there will be questions what you must answer which relate to teh proejct and your implementation.
+Each section where you will answer a question is precded by a 'Question X' header. Carefully read each quedstion and provide
+thorough answers in the following text boxes that being with 'Answer:'. Your proejct submission will be evalueated based on your answers
+to each of the questions and the implemetatino you provide.
+"""
+
+"""
+Getting Started
+
+In this project, you will analyze a dataset containing data on various customers' annual spending amounts (reported in monetary units) of
+divers product categories for internal structure. One goal of this prjoect is to best describe the variation in the different types of
+customers that a whoelsale distrubutor inteacts with. Doing so would equip the distributor with insight into how to best structure their
+delivery service to meet the needs of each customer
+
+The dataset for this proejct can be found on teh UCI Machine Learnign Repository. For the purposes of this project, teh features 'Channel'
+and 'Region' will be excluded in the analysis - with focus instead on the six product categories recodrded for customers.
+
+Run the code block below to load the wholesale customer 
+"""
